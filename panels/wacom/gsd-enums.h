c585ea7b13641b06506526a196f0ee90546e5560
Microwave <121560464+Micro856@users.noreply.github.com>
2026-05-03 22:25:46 +0200

    fix gsd to scsd

--- panels/wacom/gsd-enums.h
+++
@@ -1,7 +0,0 @@
-/* We copy scsd-wacom-device from scarecrow-settings-daemon.
- * It include "scsd-enums.h" because the include directory
- * is known. As scarecrow-settings-daemon's pkg-config file
- * prefixes this, we need a little help to avoid this
- * one line difference */
-
-#include <scarecrow-settings-daemon/scsd-enums.h>
