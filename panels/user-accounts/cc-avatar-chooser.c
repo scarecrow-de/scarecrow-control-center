3d648fa763a189fc9e2625f3383ae6370b597891
hn1f <hn1f@proton.me>
2026-05-03 09:26:01 +0200

    fix compilation error

--- panels/user-accounts/cc-avatar-chooser.c
+++ panels/user-accounts/cc-avatar-chooser.c
@@ -197,7 +197,7 @@ update_preview (GtkFileChooser               *chooser,
                 if (mime_type) {
                         pixbuf = gnome_desktop_thumbnail_factory_generate_thumbnail (thumb_factory,
                                                                                      uri,
-                                                                                     mime_type);
+                                                                                     mime_type, NULL, NULL);
                         g_free (mime_type);
                 }
 
