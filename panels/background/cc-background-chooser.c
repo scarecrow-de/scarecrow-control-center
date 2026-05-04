348c28b4ce28aed61a62f9436f9b82f4541aa8b3
Microwave <121560464+Micro856@users.noreply.github.com>
2026-05-04 14:59:02 +0200

    more rebrands :(

--- panels/background/cc-background-chooser.c
+++ panels/background/cc-background-chooser.c
@@ -237,7 +237,7 @@ on_file_chooser_response_cb (GtkDialog           *filechooser,
 
 static void
 on_file_chooser_selection_changed_cb (GtkFileChooser               *chooser,
-                                      GnomeDesktopThumbnailFactory *thumbnail_factory)
+                                      ScarecrowDesktopThumbnailFactory *thumbnail_factory)
 {
   g_autofree gchar *uri = NULL;
 
@@ -334,7 +334,7 @@ cc_background_chooser_init (CcBackgroundChooser *self)
 void
 cc_background_chooser_select_file (CcBackgroundChooser *self)
 {
-  g_autoptr(GnomeDesktopThumbnailFactory) factory = NULL;
+  g_autoptr(ScarecrowDesktopThumbnailFactory) factory = NULL;
   GtkFileFilter *filter;
   GtkWidget *filechooser;
   GtkWindow *toplevel;
